a57f1802054d14ebbc6fc18f5a85fb9eff46deec
Jozef Kamensky <jzfkamensky@gmail.com>
2021-11-01 09:40:01 +0100

    WIP letter frequencies

--- src/components/LanguageDefinition.vue
+++ src/components/LanguageDefinition.vue
@@ -1,40 +1,69 @@
 <template>
     <div>
-        <input v-model="customVowelsInput" placeholder="Custom vowels" />
-        <input v-model="customConsonantsInput" placeholder="Custom consonants" />
-        <input v-model="preferredInput" placeholder="Preferred letters" />
-        <input v-model="ommitedInput" placeholder="Custom letters" />
-        <input v-model="syllablesInput" placeholder="Syllables" />
-        <button @click="generate">Generate</button>
-        <ul>
-          <li v-for="name in names" v-bind:key="name">
-            {{ name }}
-          </li>
+      <table>
+        <tr><th>Vowel</th><th>Frequency</th><th>Actions</th></tr>
+        <tr v-for="vowel in vowels" v-bind:key="vowel.letter">
+          <!-- <td>{{ vowel.letter }}</td>
+          <td v-if="!vowel.editing">{{ vowel.frequency }}</td>
+          <td v-else></td>
+          <td><button @click="prepareToEditVowel(vowel.letter)">Edit</button></td> -->
+          <Letter :letter="vowel.letter" :frequency="vowel.frequency" @letterChanged="editVowel"/>
+        </tr>
+      </table>
+      <input v-model="syllablesInput" placeholder="Syllables" />
+      <button @click="generate">Generate</button>
+      <ul>
+        <li v-for="name in names" v-bind:key="name">
+          {{ name }}
+        </li>
         </ul>
     </div>
 </template>
 
 <script>
+import Letter from './Letter.vue'
+
+const rouletteRandom = vals => {
+  const total = vals.map(v => v.frequency).reduce((a, b) => a + b, 0)
+  const r = Math.random() * total
+  let tmp = 0
+  for (let val of vals) {
+    tmp += val.frequency
+    if (r <= tmp) {
+      return val
+    }
+  }
+}
+
 export default {
   name: 'LanguageDefinition',
+  components: {
+    Letter
+  },
   data() {
     return {
-        vowels: ["a","e","i","o","u","y"],
+        vowels: [
+          {letter: 'e', frequency: 12, editing: false},
+          {letter: 'a', frequency:  8, editing: false},
+          {letter: 'o', frequency:  8, editing: false},
+          {letter: 'i', frequency:  7, editing: false},
+          {letter: 'u', frequency:  3, editing: false},
+          {letter: 'y', frequency:  2, editing: false},
+          ],
         consonants: ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"],
-        customVowelsInput: '',
-        customConsonantsInput: '',
-        preferredInput: '',
-        ommitedInput: '',
         syllablesInput: '',
         names: [],
     }
   },
   methods: {
+    editVowel({ letter, frequency }) {
+      const newVowels = this.vowels.slice()
+      const i = newVowels.map(v => v.letter).indexOf(letter)
+      newVowels[i].frequency = frequency
+      newVowels[i].editing = false
+      this.vowels = newVowels
+    },
     generate() {
-      // const customVowels = this.customVowelsInput.split(',')
-      // const customConsonants = this.customConsonantsInput.split(',')
-      // const preferred = this.preferredInput.split(', ')
-      // const ommitted = this.ommitedLetters.split(',')
       const syllables = this.syllablesInput.split(',')
       for (let i = 0; i < 10; i++) {
         const l = Math.floor(Math.random() * 3) + 2
@@ -43,16 +72,14 @@ export default {
           const s = syllables[Math.floor(Math.random() * syllables.length)]
           res += s.split('').map(ch => {
             if (ch === 'v') {
-              return this.vowels[Math.floor(Math.random() * this.vowels.length)]
+              return rouletteRandom(this.vowels).letter
             }
             return this.consonants[Math.floor(Math.random() * this.consonants.length)]
           }).join('')
         }
-        console.log(res)
         this.names.push(res)
       }
-      console.log(this.names)
-    }
+    },
   }
 }
 </script>
---
+++ src/components/Letter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="editing">
+    <span>{{ letter }}</span>
+    <input type="number" v-model="input" placeholder="Frequency" />
+    <button @click="save">Save</button>
+  </div>
+  <div v-else>
+    <span>{{ letter }}</span>
+    <span>{{ frequency }}</span>
+    <button @click="toEdit">Edit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Letter',
+  props: ['letter', 'frequency'],
+    data() {
+      return {
+        input: this.frequency,
+        editing: false,
+      }
+  },
+  emits: ['letterChanged'],
+  methods: {
+    toEdit() {
+      this.editing = true
+    },
+    save() {
+      this.editing = false
+      console.log('emit event')
+      this.$emit('letterChanged', { letter: this.letter, frequency: this.input })
+    }
+  },
+}
+</script>
+
+<style>
+</style>
